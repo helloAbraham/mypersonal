bc63f729d29211ad1a241fca2f8bd8cdb94ee55a
loveiskind22 <parkerfabraham@gmail.com>
2023-12-12 23:53:10 -0600

    this is the first commit

---
+++ src/main/java/com/mypersonal/MypersonalApplication.java
@@ -0,0 +1,13 @@
+package com.mypersonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypersonalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MypersonalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mypersonal/MypersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypersonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypersonalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

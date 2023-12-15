0b5d3204edc519cc3fa344bb24579930a7b95818
loveiskind22 <parkerfabraham@gmail.com>
2023-12-14 20:35:33 -0600

    added

--- src/main/java/com/mypersonal/MypersonalApplication.java
+++ src/main/java/com/mypersonal/MypersonalApplication.java
@@ -1,13 +1,23 @@
 package com.mypersonal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MypersonalApplication {
+public class MypersonalApplication implements CommandLineRunner {
+
+    public static final Logger logger = LoggerFactory.getLogger(MypersonalApplication.class);
 
     public static void main(String[] args) {
+        logger.info("It's a continuous integration job  ");
         SpringApplication.run(MypersonalApplication.class, args);
     }
 
+    @Override
+    public void run (String... args){
+        logger.info("It's my second logger");
+    }
 }
--- src/test/java/com/mypersonal/MypersonalApplicationTests.java
+++ src/test/java/com/mypersonal/MypersonalApplicationTests.java
@@ -1,13 +1,25 @@
 package com.mypersonal;
 
+
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
+
 
 @SpringBootTest
 class MypersonalApplicationTests {
 
+    public static final Logger logger = LoggerFactory.getLogger(MypersonalApplicationTests.class);
     @Test
     void contextLoads() {
+        logger.info("its a test class");
+        assertEquals(true, true);
     }
+
+
 
 }
